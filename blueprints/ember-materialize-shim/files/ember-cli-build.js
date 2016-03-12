20cde876a3cc6aa53526db385393e7da4b012230
Mike North <michael.l.north@gmail.com>
2016-03-12 14:34:54 +0800

    blueprint fixes

--- blueprints/ember-materialize-shim/files/ember-cli-build.js
+++ blueprints/ember-materialize-shim/files/ember-cli-build.js
@@ -4,6 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    // Add options here
     sassOptions: {
       includePaths: [
         'bower_components/Materialize/sass'
