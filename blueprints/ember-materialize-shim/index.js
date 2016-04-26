2b0d09c518f395e25e0a8f297efed4edf1189fb5
Mike North <michael.l.north@gmail.com>
2016-04-26 17:17:04 +0200

    Fix installation blueprint

--- blueprints/ember-materialize-shim/index.js
+++ blueprints/ember-materialize-shim/index.js
@@ -1,21 +1,16 @@
 /*jshint node:true*/
 module.exports = {
-  description: '',
+  description: 'ember-materialize-shim installation blueprint',
   normalizeEntityName: function() {},
-  beforeInstall: function(options) {
-    return this.addBowerPackageToProject('materialize', '0.97.6');
-  },
 
-  // locals: function(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
-
-  afterInstall: function(options) {
-    return this.addPackageToProject('ember-cli-sass', 'mike-north/ember-cli-sass#8ceb57d41f5774e8ececb5d1f05454449c19000c').then(function() {
-      return this.addAddonToProject('ember-material-design-icons-shim', '~0.1.2');
+  beforeInstall: function() {
+    return this.addBowerPackageToProject('materialize', '0.97.6').then(function() {
+      return this.addAddonsToProject({
+        packages: [
+          { name: 'ember-cli-sass', target: 'mike-north/ember-cli-sass#8ceb57d41f5774e8ececb5d1f05454449c19000c' },
+          { name: 'ember-material-design-icons-shim', target: '~0.1.3' }
+        ]
+      });
     }.bind(this));
   }
 };
