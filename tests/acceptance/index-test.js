c071bcbf86c782df02334dcc8ec84f681219f1df
Mike North <michael.l.north@gmail.com>
2016-07-02 11:11:20 -0700

    acceptance test fix

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -9,7 +9,7 @@ test('Installation verification', function(assert) {
 
   andThen(function() {
     assert.ok(find('#title').css('font-family').indexOf('Roboto') >= 0, 'Stylesheet is installed');
-    assert.equal(find('.material-icons').css('font-family'), '\"Material Icons\"', 'Icons are installed');
+    assert.equal(find('.material-icons').css('font-family'), '\'Material Icons\'', 'Icons are installed');
     assert.equal(typeof Materialize, 'object', 'JS is installed as an ES6 module');
     assert.equal(currentURL(), '/');
   });
