87d45484ae86bf822888028fbbcbde1e76b60ba3
Mike North <michael.l.north@gmail.com>
2016-08-09 19:06:24 +0530

    Update materialize to 0.97.7

--- blueprints/ember-materialize-shim/index.js
+++ blueprints/ember-materialize-shim/index.js
@@ -4,7 +4,7 @@ module.exports = {
   normalizeEntityName: function() {},
 
   beforeInstall: function() {
-    return this.addBowerPackageToProject('materialize', '0.97.6').then(function() {
+    return this.addBowerPackageToProject('materialize', '0.97.7').then(function() {
       return this.addAddonsToProject({
         packages: [{
           name: 'ember-cli-sass',
